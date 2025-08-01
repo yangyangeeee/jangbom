67f53668e1a7c9d62de40e0d472c52ab647cbe59
hyunnbin8 <bin1025yh@gmail.com>
2025-08-01 23:42:47 +0900

    요리 검색 후 ai 분석 기능

--- food/models.py
+++ food/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    category = models.ManyToManyField(to=Category, through='IngredientCategory', related_name='ingredients')
+    image = models.ImageField(upload_to='ingredients/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class IngredientCategory(models.Model):
+    ingredient = models.ForeignKey(to=Ingredient, on_delete=models.CASCADE, related_name='ingredient_categories')
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='ingredient_categories')
+
+    def __str__(self):
+        return f"{self.ingredient.name} in {self.category.name}"
+    
+class ShoppingList(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    destination = models.CharField(max_length=100, default="미지정")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.destination} - {self.created_at.date()}"
+
+class ShoppingListIngredient(models.Model):
+    shopping_list = models.ForeignKey(to=ShoppingList, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(to=Ingredient, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.shopping_list.id} - {self.ingredient.name}"
--- food/urls.py
+++ food/urls.py
@@ -5,4 +5,10 @@ app_name = 'food'
 
 urlpatterns = [
     path('', main, name='main'),
+    path('recipe/start/', recipe_input_view, name='recipe_input'),
+    path('recipe/ingredients/', recipe_ingredient_result, name='recipe_ingredients'),
+    path('recipe/confirm/', confirm_shopping_list, name='confirm_shopping_list'),
+    path('recipe/result/', shopping_list_result, name='shopping_list_result'),
+    path('chat/', chat_with_gpt, name='chat'),
+    path('recipe/search/', ingredient_search_view, name='ingredient_search'),
 ]
